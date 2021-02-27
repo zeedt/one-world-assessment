e1e6732d9d838264d4c31b6368bcb94e418f588e
zeedt <yusufsaheedtaiwo@gmail.com>
2021-02-27 16:08:12 +0100

    first commit

---
+++ src/main/java/com/zeed/one/world/assessment/OneWorldAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.zeed.one.world.assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneWorldAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneWorldAssessmentApplication.class, args);
+    }
+
+}
