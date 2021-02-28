96ec5683bf2216198762154b1d5515a766d92e57
zeedt <yusufsaheedtaiwo@gmail.com>
2021-02-28 23:04:39 +0100

    mail verification implemented

--- src/main/java/com/zeed/one/world/assessment/config/SmtpConfig.java
+++ src/main/java/com/zeed/one/world/assessment/config/SmtpConfig.java
@@ -1,6 +1,7 @@
 package com.zeed.one.world.assessment.config;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -11,14 +12,21 @@ import java.util.Properties;
 @Configuration
 public class SmtpConfig {
 
+    @Value("${smtp.username:assessment.sy@gmail.com}")
+    private String smtpUsername;
+
+    @Value("${smtp.password:Assessment2.}")
+    private String smtpPassword;
+
+
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("assessment.sy@gmail.com");
-        mailSender.setPassword("Assessment2.");
+        mailSender.setUsername(smtpUsername);
+        mailSender.setPassword(smtpPassword);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
